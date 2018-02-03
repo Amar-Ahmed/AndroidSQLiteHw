47a4813286343976ce5e4bc5de7b4e1219980f5d
Amar Ahmed <amar11432@gmail.com>
2018-02-03 13:14:14 -0500

    first commit

---
+++ app/src/main/java/com/example/android/androidsqlitehw/Product.java
@@ -0,0 +1,8 @@
+package com.example.android.androidsqlitehw;
+
+/**
+ * Created by C4Q on 2/3/18.
+ */
+
+public class Product {
+}
---
+++ app/src/main/java/com/example/android/androidsqlitehw/database/SqliteDatabase.java
@@ -0,0 +1,8 @@
+package com.example.android.androidsqlitehw.database;
+
+/**
+ * Created by C4Q on 2/3/18.
+ */
+
+public class SqliteDatabase {
+}
